63a4fb391019ccd6d816a2641eafdb093bf193b1
abrahamomolewa <abrahamomolewa@gmail.com>
2023-08-03 16:11:54 +0100

    added more stuffs

---
+++ src/modular_arithmestic1.rs
@@ -0,0 +1,20 @@
+/*
+
+Calculate the following integers:
+
+11 ≡ x mod 6
+8146798528947 ≡ y mod 17
+ */
+
+
+ fn main() {
+
+
+    let x = 11 % 6;
+ 
+    let y: i64 = 8146798528947 % 17;
+ 
+    println!("{}", x);
+ 
+    println!("{}", y);
+ }
